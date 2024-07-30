db7d4eff567a05ff3b4583e1a08e1d3a966643ca
Riya <Riya@DESKTOP-EC7SIAC>
2024-07-30 07:53:51 +0530

    MErko Github aya

--- Project/src/Pack/Hola.java
+++ Project/src/Pack/Hola.java
@@ -3,9 +3,7 @@ package Pack;
 public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Github1");
-		System.out.println("Github2");
-		System.out.println("Github3");
-		System.out.println("GIthub4");
 		System.out.println("hello");
+		
 	}
 }
